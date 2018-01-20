31f14167e3249746e47c65413bf33d1a72720ac4
Lesly Arun Franco <lesly.franco@astrazeneca.com>
2018-01-20 23:58:45 +0530

    code which will return test results in dialogflow bot

--- app.py
+++ app.py
@@ -15,13 +15,17 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+copiedtracker = []
+scoretracker = []
+finaltext = []
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+    # print("Request:")
+    # print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
@@ -34,16 +38,65 @@ def webhook():
 
 def processRequest(req):
     if req.get("queryResult").get("action") == "qs":
-        res = interview_qs()
+        res = interview_qs(req)
+        print("response is", res)
         return res
+
+    elif req.get("queryResult").get("action") == "bye":
+        for index, item in enumerate(copiedtracker):
+            finaltext.append("""Here are the Test Results:
+                        Qs.""" + (index + 1) + """ - Answer copied - """ + item)
+            print("""Here are the Test Results:
+            Qs.""" + (index + 1) + """ - Answer copied - """ + item)
+
+        for index, item in enumerate(scoretracker):
+            finaltext.append("""Score for every answer:
+             Qs.""" + (index + 1) + """ - Score is """ + item)
+            print("""Score for every answer:
+             Qs.""" + (index + 1) + """ - Score is """ + item)
+
+        return {
+            "fulfillmentText": finaltext
+        }
     else:
         return {}
 
 
-def interview_qs():
-    qs = bot.random_question(bot.random_qs_index())
+def interview_qs(req):
+    qs_idx = bot.random_qs_index()
+    copiedtracker.append(copiedornot(req, qs_idx))
+    scoretracker.append(answerscore(req, qs_idx))
+    qs = bot.random_question(qs_idx)
     return {
         "fulfillmentText": qs
+    }
+
+
+def copiedornot(req, qs_idx):
+    qs = bot.random_question(qs_idx)
+    links = bot.google_links(qs)
+    texts = bot.get_link_text(links)
+    answer = req.get("queryResult").get("queryText")
+    copied = bot.is_copied(texts, answer)
+    if copied:
+        print("Answer copied from Google")
+    return copied
+
+
+def answerscore(req, qs_idx):
+    qs = bot.random_question(qs_idx)
+    answer = req.get("queryResult").get("queryText")
+    ans_tokens = bot.answer_tokens(answer)
+    orig_ans_tokens = bot.original_ans_tokens()
+    simscore = bot.similarity_check(ans_tokens, orig_ans_tokens)
+    keywords = bot.key_matching(qs_idx)
+    keysimscore = bot.key_similarity_check(keywords, ans_tokens)
+    matches = bot.match_score(keywords, ans_tokens)
+    alternatescore = bot.alternate_score(keywords, matches)
+    return {
+        "similarityScore": simscore,
+        "keySimilarityScore": keysimscore,
+        "alternateScore": alternatescore
     }
 
 
